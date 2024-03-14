5be858627299860c5cc8c67a028a48ac3b2aac89
Gabrielstivenn <gabrielxeroso10@gmail.com>
2024-03-13 21:58:25 -0300

    tarefas FOR

---
+++ src/main/java/com/mycompany/revisaofor/RevisaoFOR.java
@@ -0,0 +1,49 @@
+
+package com.mycompany.revisaofor;
+
+import java.util.Scanner;
+
+public class RevisaoFOR {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner (System.in);
+        
+        /*int n =10;
+        int soma = 0;
+        soma = teclado.nextInt();
+
+        for(int i = 2; i<= n; i+=2){
+        soma += i;
+        System.out.println(i);
+    }
+        System.out.println("A soma dos numeros pares até" + n + "é:" +soma );
+    */
+        //psvm tab
+        
+        /*int n = 10;
+        int primeirotermo = 0;
+        int segundotermo= 1;
+        
+        System.out.println("os primeiro numeros" + n + "numeros da sequnecia de fibonacci são:");
+        System.out.println(primeirotermo + " " + segundotermo + " ");
+        
+        for (int i = 3; i <= n; i++){
+            int proximotermo = primeirotermo + segundotermo;
+            System.out.println(proximotermo +" ");
+            
+            proximotermo = segundotermo;
+            segundotermo = proximotermo; */
+        
+        /*for(int i = 9; i >= i; i-=2){
+            System.out.println(i);
+*/
+int n =15;   
+int x = 1;
+double soma =0;
+    for(int i=0; i<= n; i ++){
+        System.out.printf("\n%d",i);
+        }
+        }
+        
+    }
+
